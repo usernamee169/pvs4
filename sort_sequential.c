7dc2657c21475643c8fc9905457d25491f64c042
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-14 17:18:33 +0500

    Update sort_sequential.c

--- sort_sequential.c
+++ sort_sequential.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define N 1000000
+
 void merge(float *arr, int l, int m, int r) {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    float *L = (float *)malloc(n1 * sizeof(float));
-    float *R = (float *)malloc(n2 * sizeof(float));
+    float *L = (float*)malloc(n1 * sizeof(float));
+    float *R = (float*)malloc(n2 * sizeof(float));
 
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
@@ -18,6 +20,7 @@ void merge(float *arr, int l, int m, int r) {
     i = 0;
     j = 0;
     k = l;
+    
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
@@ -54,41 +57,25 @@ void mergeSort(float *arr, int l, int r) {
     }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <array_size>\n", argv[0]);
-        return 1;
-    }
-
-    int N = atoi(argv[1]);
-    if (N <= 0) {
-        printf("Array size must be positive\n");
-        return 1;
-    }
-
-    float *array = (float *)malloc(N * sizeof(float));
+int main() {
+    float *array = (float*)malloc(N * sizeof(float));
     
-    srand(time(NULL));
+    // Инициализация массива
     for (int i = 0; i < N; i++) {
         array[i] = (float)rand() / RAND_MAX;
     }
-
+    
     clock_t start = clock();
+    
     mergeSort(array, 0, N - 1);
+    
     clock_t end = clock();
-
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Sequential merge sort completed\n");
-    printf("Time: %f seconds\n", time_spent);
-
-    // Проверка сортировки (опционально)
-    for (int i = 0; i < N - 1; i++) {
-        if (array[i] > array[i + 1]) {
-            printf("Sorting error at index %d\n", i);
-            break;
-        }
-    }
-
+    double time = (double)(end - start) / CLOCKS_PER_SEC;
+    
+    printf("First element: %f\n", array[0]);
+    printf("Last element: %f\n", array[N-1]);
+    printf("Time: %f seconds\n", time);
+    
     free(array);
     return 0;
 }
