89767cf87b09d0c839efcb4aa17a0ef40414ff33
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-14 17:19:25 +0500

    Update array_ops_sequential.c

--- array_ops_sequential.c
+++ array_ops_sequential.c
@@ -2,53 +2,41 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <array_size>\n", argv[0]);
-        return 1;
-    }
+#define N 1000000
 
-    int N = atoi(argv[1]);
-    if (N <= 0) {
-        printf("Array size must be positive\n");
-        return 1;
-    }
-
-    float *A = (float *)malloc(N * sizeof(float));
-    float *B = (float *)malloc(N * sizeof(float));
-    float *add = (float *)malloc(N * sizeof(float));
-    float *sub = (float *)malloc(N * sizeof(float));
-    float *mul = (float *)malloc(N * sizeof(float));
-    float *div = (float *)malloc(N * sizeof(float));
+int main() {
+    float *a = (float*)malloc(N * sizeof(float));
+    float *b = (float*)malloc(N * sizeof(float));
+    float *add = (float*)malloc(N * sizeof(float));
+    float *sub = (float*)malloc(N * sizeof(float));
+    float *mul = (float*)malloc(N * sizeof(float));
+    float *div = (float*)malloc(N * sizeof(float));
     
-    srand(time(NULL));
+    // Инициализация массивов
     for (int i = 0; i < N; i++) {
-        A[i] = (float)rand() / RAND_MAX;
-        B[i] = (float)rand() / RAND_MAX + 0.1f; // Чтобы избежать деления на 0
+        a[i] = (float)rand() / RAND_MAX + 0.1f; // +0.1 чтобы избежать деления на 0
+        b[i] = (float)rand() / RAND_MAX + 0.1f;
     }
-
+    
     clock_t start = clock();
+    
+    // Операции с массивами
     for (int i = 0; i < N; i++) {
-        add[i] = A[i] + B[i];
-        sub[i] = A[i] - B[i];
-        mul[i] = A[i] * B[i];
-        div[i] = A[i] / B[i];
+        add[i] = a[i] + b[i];
+        sub[i] = a[i] - b[i];
+        mul[i] = a[i] * b[i];
+        div[i] = a[i] / b[i];
     }
+    
     clock_t end = clock();
-
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Sequential array operations completed\n");
-    printf("Time: %f seconds\n", time_spent);
-
-    // Пример вывода первых 5 элементов
-    printf("Sample results (first 5 elements):\n");
-    for (int i = 0; i < 5 && i < N; i++) {
-        printf("%f + %f = %f\n", A[i], B[i], add[i]);
-        printf("%f - %f = %f\n", A[i], B[i], sub[i]);
-        printf("%f * %f = %f\n", A[i], B[i], mul[i]);
-        printf("%f / %f = %f\n", A[i], B[i], div[i]);
-    }
-
-    free(A); free(B); free(add); free(sub); free(mul); free(div);
+    double time = (double)(end - start) / CLOCKS_PER_SEC;
+    
+    printf("First add result: %f\n", add[0]);
+    printf("First sub result: %f\n", sub[0]);
+    printf("First mul result: %f\n", mul[0]);
+    printf("First div result: %f\n", div[0]);
+    printf("Time: %f seconds\n", time);
+    
+    free(a); free(b); free(add); free(sub); free(mul); free(div);
     return 0;
 }
