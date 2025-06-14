fddc4366888806ce9541c7ab892b4682506bc7ca
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-14 17:37:08 +0500

    Update array_ops_sequential.c

--- array_ops_sequential.c
+++ array_ops_sequential.c
@@ -12,15 +12,13 @@ int main() {
     float *mul = (float*)malloc(N * sizeof(float));
     float *div = (float*)malloc(N * sizeof(float));
     
-    // Инициализация массивов
     for (int i = 0; i < N; i++) {
-        a[i] = (float)rand() / RAND_MAX + 0.1f; // +0.1 чтобы избежать деления на 0
+        a[i] = (float)rand() / RAND_MAX + 0.1f; 
         b[i] = (float)rand() / RAND_MAX + 0.1f;
     }
     
     clock_t start = clock();
     
-    // Операции с массивами
     for (int i = 0; i < N; i++) {
         add[i] = a[i] + b[i];
         sub[i] = a[i] - b[i];
@@ -31,11 +29,7 @@ int main() {
     clock_t end = clock();
     double time = (double)(end - start) / CLOCKS_PER_SEC;
     
-    printf("First add result: %f\n", add[0]);
-    printf("First sub result: %f\n", sub[0]);
-    printf("First mul result: %f\n", mul[0]);
-    printf("First div result: %f\n", div[0]);
-    printf("Time: %f seconds\n", time);
+    printf("Время: %f seconds\n", time);
     
     free(a); free(b); free(add); free(sub); free(mul); free(div);
     return 0;
