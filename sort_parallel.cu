2a87c95523497178c924a02f5ae0a071d8f8473c
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-14 17:44:19 +0500

    Update sort_parallel.cu

--- sort_parallel.cu
+++ sort_parallel.cu
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <cuda_runtime.h>
 
-#define N 1048576  // Должно быть степенью 2 для bitonic sort
+#define N1 131072   // 2^17
+#define N2 524288   // 2^19
+#define N3 1048576  // 2^20
+
 #define THREADS 256
 
 __device__ void swap(float &a, float &b) {
@@ -29,12 +32,11 @@ __global__ void bitonicSortStep(float *devValues, int j, int k) {
     }
 }
 
-int main() {
-    float *h_values = (float*)malloc(N * sizeof(float));
+void testSort(int size) {
+    float *h_values = (float*)malloc(size * sizeof(float));
     float *d_values;
     
-    // Инициализация массива
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < size; i++) {
         h_values[i] = (float)rand() / RAND_MAX;
     }
     
@@ -42,15 +44,15 @@ int main() {
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     
-    cudaMalloc(&d_values, N * sizeof(float));
-    cudaMemcpy(d_values, h_values, N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMalloc(&d_values, size * sizeof(float));
+    cudaMemcpy(d_values, h_values, size * sizeof(float), cudaMemcpyHostToDevice);
     
-    int blocks = (N + THREADS - 1) / THREADS;
+    int blocks = (size + THREADS - 1) / THREADS;
     
     cudaEventRecord(start);
     
     int j, k;
-    for (k = 2; k <= N; k <<= 1) {
+    for (k = 2; k <= size; k <<= 1) {
         for (j = k >> 1; j > 0; j >>= 1) {
             bitonicSortStep<<<blocks, THREADS>>>(d_values, j, k);
         }
@@ -59,17 +61,33 @@ int main() {
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     
-    cudaMemcpy(h_values, d_values, N * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_values, d_values, size * sizeof(float), cudaMemcpyDeviceToHost);
     
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, stop);
     
-    printf("First element: %f\n", h_values[0]);
-    printf("Last element: %f\n", h_values[N-1]);
-    printf("Time: %f seconds\n", milliseconds / 1000.0f);
+    int sorted = 1;
+    for (int i = 0; i < size - 1; i++) {
+        if (h_values[i] > h_values[i+1]) {
+            sorted = 0;
+            break;
+        }
+    }
+    
+    printf("Размер массива: %d\n", size);
+    printf("Время: %f seconds\n\n", milliseconds / 1000.0f);
     
     cudaFree(d_values);
     free(h_values);
+}
+
+int main() {
+    srand(time(NULL));
+    
+    printf("Параллельная сортировка:\n");
+    testSort(N1);
+    testSort(N2);
+    testSort(N3);
     
     return 0;
 }
