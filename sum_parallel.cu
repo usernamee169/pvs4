3e72415cb199837a6ff2cee5cbab3f92e6c986f7
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-14 15:38:30 +0500

    Update sum_parallel.cu

--- sum_parallel.cu
+++ sum_parallel.cu
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <cuda_runtime.h>
 
-__global__ void sumKernel(float *array, float *result, int n) {
+__global__ void sumArray(float *array, float *result, int N) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < n) {
+    if (idx < N) {
         atomicAdd(result, array[idx]);
     }
 }
@@ -14,46 +14,55 @@ int main(int argc, char *argv[]) {
         printf("Usage: %s <array_size>\n", argv[0]);
         return 1;
     }
-    
-    int n = atoi(argv[1]);
-    float *h_array = (float*)malloc(n * sizeof(float));
-    float *d_array, *d_result;
-    float h_result = 0.0;
-    
-    for(int i = 0; i < n; i++) {
-        h_array[i] = 1.0;
+
+    int N = atoi(argv[1]);
+    if (N <= 0) {
+        printf("Array size must be positive\n");
+        return 1;
     }
+
+    float *h_array = (float *)malloc(N * sizeof(float));
+    float *d_array, *d_result;
+    float h_result = 0.0f;
     
-    cudaMalloc((void**)&d_array, n * sizeof(float));
-    cudaMalloc((void**)&d_result, sizeof(float));
-    cudaMemcpy(d_array, h_array, n * sizeof(float), cudaMemcpyHostToDevice);
+    // Инициализация массива
+    srand(time(NULL));
+    for (int i = 0; i < N; i++) {
+        h_array[i] = (float)rand() / RAND_MAX;
+    }
+
+    // Выделение памяти на устройстве
+    cudaMalloc(&d_array, N * sizeof(float));
+    cudaMalloc(&d_result, sizeof(float));
+    cudaMemcpy(d_array, h_array, N * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_result, &h_result, sizeof(float), cudaMemcpyHostToDevice);
-    
+
+    // Запуск ядра
     int blockSize = 256;
-    int gridSize = (n + blockSize - 1) / blockSize;
+    int numBlocks = (N + blockSize - 1) / blockSize;
     
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start);
-    
-    sumKernel<<<gridSize, blockSize>>>(d_array, d_result, n);
-    
+
+    sumArray<<<numBlocks, blockSize>>>(d_array, d_result, N);
+
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
-    
-    cudaMemcpy(&h_result, d_result, sizeof(float), cudaMemcpyDeviceToHost);
-    
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, stop);
-    
-    printf("Array size: %d\n", n);
-    printf("Sum: %f\n", h_result);
-    printf("Time: %f ms\n", milliseconds);
-    
+
+    // Копирование результата обратно
+    cudaMemcpy(&h_result, d_result, sizeof(float), cudaMemcpyDeviceToHost);
+
+    printf("Parallel sum: %f\n", h_result);
+    printf("Time: %f seconds\n", milliseconds / 1000);
+
+    // Освобождение памяти
     cudaFree(d_array);
     cudaFree(d_result);
     free(h_array);
-    
+
     return 0;
 }
