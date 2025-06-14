44e8f83dd6b92242b11bdc656eddb1b332e5da5b
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-14 17:18:50 +0500

    Update sort_parallel.cu

--- sort_parallel.cu
+++ sort_parallel.cu
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <cuda_runtime.h>
 
-__device__ void swap(float *a, float *b) {
-    float t = *a;
-    *a = *b;
-    *b = t;
+#define N 1048576  // Должно быть степенью 2 для bitonic sort
+#define THREADS 256
+
+__device__ void swap(float &a, float &b) {
+    float t = a;
+    a = b;
+    b = t;
 }
 
 __global__ void bitonicSortStep(float *devValues, int j, int k) {
@@ -15,77 +18,58 @@ __global__ void bitonicSortStep(float *devValues, int j, int k) {
     if (ixj > i) {
         if ((i & k) == 0) {
             if (devValues[i] > devValues[ixj]) {
-                swap(&devValues[i], &devValues[ixj]);
+                swap(devValues[i], devValues[ixj]);
             }
         }
         if ((i & k) != 0) {
             if (devValues[i] < devValues[ixj]) {
-                swap(&devValues[i], &devValues[ixj]);
+                swap(devValues[i], devValues[ixj]);
             }
         }
     }
 }
 
-void bitonicSort(float *values, int N) {
-    float *devValues;
-    cudaMalloc(&devValues, N * sizeof(float));
-    cudaMemcpy(devValues, values, N * sizeof(float), cudaMemcpyHostToDevice);
-
-    int threads = 256;
-    int blocks = (N + threads - 1) / threads;
-
-    for (int k = 2; k <= N; k <<= 1) {
-        for (int j = k >> 1; j > 0; j >>= 1) {
-            bitonicSortStep<<<blocks, threads>>>(devValues, j, k);
-        }
-    }
-
-    cudaMemcpy(values, devValues, N * sizeof(float), cudaMemcpyDeviceToHost);
-    cudaFree(devValues);
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <array_size>\n", argv[0]);
-        return 1;
-    }
-
-    int N = atoi(argv[1]);
-    if (N <= 0 || (N & (N - 1)) != 0) {
-        printf("Array size must be positive and power of 2\n");
-        return 1;
-    }
-
-    float *array = (float *)malloc(N * sizeof(float));
+int main() {
+    float *h_values = (float*)malloc(N * sizeof(float));
+    float *d_values;
     
-    srand(time(NULL));
+    // Инициализация массива
     for (int i = 0; i < N; i++) {
-        array[i] = (float)rand() / RAND_MAX;
+        h_values[i] = (float)rand() / RAND_MAX;
     }
-
+    
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
+    
+    cudaMalloc(&d_values, N * sizeof(float));
+    cudaMemcpy(d_values, h_values, N * sizeof(float), cudaMemcpyHostToDevice);
+    
+    int blocks = (N + THREADS - 1) / THREADS;
+    
     cudaEventRecord(start);
-
-    bitonicSort(array, N);
-
-    cudaEventRecord(stop);
-    cudaEventSynchronize(stop);
-    float milliseconds = 0;
-    cudaEventElapsedTime(&milliseconds, start, stop);
-
-    printf("Parallel bitonic sort completed\n");
-    printf("Time: %f seconds\n", milliseconds / 1000);
-
-    // Проверка сортировки
-    for (int i = 0; i < N - 1; i++) {
-        if (array[i] > array[i + 1]) {
-            printf("Sorting error at index %d\n", i);
-            break;
+    
+    int j, k;
+    for (k = 2; k <= N; k <<= 1) {
+        for (j = k >> 1; j > 0; j >>= 1) {
+            bitonicSortStep<<<blocks, THREADS>>>(d_values, j, k);
         }
     }
-
-    free(array);
+    
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    
+    cudaMemcpy(h_values, d_values, N * sizeof(float), cudaMemcpyDeviceToHost);
+    
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    
+    printf("First element: %f\n", h_values[0]);
+    printf("Last element: %f\n", h_values[N-1]);
+    printf("Time: %f seconds\n", milliseconds / 1000.0f);
+    
+    cudaFree(d_values);
+    free(h_values);
+    
     return 0;
 }
