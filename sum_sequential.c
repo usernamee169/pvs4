4a158ad4b1b92d554d22966d68b79f6902e27959
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-14 17:33:23 +0500

    Update sum_sequential.c

--- sum_sequential.c
+++ sum_sequential.c
@@ -8,14 +8,12 @@ int main() {
     float *array = (float*)malloc(N * sizeof(float));
     float sum = 0.0f;
     
-    // Инициализация массива
     for (int i = 0; i < N; i++) {
         array[i] = (float)rand() / RAND_MAX;
     }
     
     clock_t start = clock();
     
-    // Последовательное суммирование
     for (int i = 0; i < N; i++) {
         sum += array[i];
     }
@@ -23,8 +21,7 @@ int main() {
     clock_t end = clock();
     double time = (double)(end - start) / CLOCKS_PER_SEC;
     
-    printf("Sum: %f\n", sum);
-    printf("Time: %f seconds\n", time);
+    printf("Время: %f seconds\n", time);
     
     free(array);
     return 0;
